f24d9b47156a80092257200c9ffa48a2daf040e7
Julie C <jucheng925@gmail.com>
2023-04-05 13:09:29 -0400

    scoping maddness

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName;
+customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    //return console.log(bestCustomer)
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    //return console.log(bestCustomer)
+}
+
+const leastFavoriteCustomer = 'rob'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe not rob'
+    //return console.log(leastFavoriteCustomer)
+}
+//changeLeastFavoriteCustomer()
